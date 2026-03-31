188d1ff31020dc8da1c0a5cdfcea95b90040fab3
mathew-fleisch <mathew.fleisch@gmail.com>
2026-03-31 13:17:46 -0700

    Use standard numbers for dice instead of Unicode runes

--- main.go
+++ main.go
@@ -3352,14 +3352,6 @@ func handlePlayback(state GameState, topo Topology) {
 }
 
 func toDice(n int) string {
-	switch n {
-	case 1: return "⚀"
-	case 2: return "⚁"
-	case 3: return "⚂"
-	case 4: return "⚃"
-	case 5: return "⚄"
-	case 6: return "⚅"
-	}
 	return fmt.Sprintf("%d", n)
 }
 
